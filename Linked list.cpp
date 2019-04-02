38f0e8f91858e15dad8561227dca34871069cd23
nianuratika <48706479+nianuratika@users.noreply.github.com>
2019-04-02 20:23:44 +0700

    Delete Linked list.cpp

--- Linked list.cpp
+++
@@ -1,7 +0,0 @@
-// Desta Feranita       1517051002
-// Nia Nur Atika        1817051015
-// Eka Intan Sari       1817051022
-// Rena Okriyani        1817051029
-
-#include <iostream>
-using namespace std;
