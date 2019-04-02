cadd6227384a0ba846ea323019bb17e61619c173
destaferanita <38813643+destaferanita@users.noreply.github.com>
2019-04-02 21:03:00 +0700

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -11,3 +11,76 @@ struct Node {
   int data ;
   Node *next;
 };
+
+int panjang;
+Node *head = NULL;
+
+void iDepan (int databaru){
+  Node *baru, *bantu;
+  baru = new Node;
+  baru->data = databaru;
+  
+  if (head ==NULL){
+    head = baru;
+    head ->next = head ;
+  }
+  
+  else {
+    bantu = head ;
+    for (int i=0; i<panjang; i++) {
+      if (bantu->next == head){
+        baru->next= head;
+        head = baru;
+        bantu->next = head;
+        break;}
+        bantu = bantu->next;
+    }
+  }
+  panjang ++;
+}
+
+void iBelakang (int dataBaru){
+  Node *baru, *bantu;
+  baru = new Node ;
+  baru->data = databaru;
+  
+  if(head == NULL){
+    head = baru ;
+    head->next = head ;
+  }
+  
+  else {
+    bantu = head;
+    for (int i=0; i<panjang; i++){
+      if (bantu->next== head){
+        bantu->next = baru;
+        baru->next = head;
+        break;
+      }
+      bantu = bantu->next;
+    }
+  }
+  panjang++;
+}
+
+void hDepan(){
+  Node *baru;
+  bantu = head ;
+  if (panjang == 0){
+    cout<<"Data kosong \n";
+  }
+  else if (panjang ==1){
+    head = NULL;
+  }
+  else {
+    for (int i=0; i<panjang; i++){
+      if (bantu->next == head){
+        head = head->next;
+        bantu->next = head ;
+        break;
+      }
+      bantu = bantu->next;
+    }
+  }
+  panjang --;
+}
