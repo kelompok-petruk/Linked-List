5a24f51e365f3e7289b9a0ffa1dedf209b85b1f5
ekaintansari <48711374+ekaintansari@users.noreply.github.com>
2019-04-03 09:26:03 +0700

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,7 +1,9 @@
-// Desta Feranita      1517051002
-// Nia Nur Atika       1817051015
-// Eka Intan Sari      1817051022
-// Rena Okriyani       1817051029
+// KELOMPOK        <<"""""DEAR""""">>
+
+//                 1. Desta Feranita      1517051002
+//                 2. Nia Nur Atika       1817051015
+//                 3. Eka Intan Sari      1817051022
+//                 4. Rena Okriyani       1817051029
 
 
 #include <iostream>
