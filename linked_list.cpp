6d9ad6173e01da608f3d02cd0e09f57714fe4f85
renaokriyani29 <48759331+renaokriyani29@users.noreply.github.com>
2019-04-03 09:22:17 +0700

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,7 +1,7 @@
-//Desta Feranita      1517051002
-//Nia Nur Atika       1817051015
-//Eka Intan Sari      1817051022
-//Rena Okriyani       1817051029
+// Desta Feranita      1517051002
+// Nia Nur Atika       1817051015
+// Eka Intan Sari      1817051022
+// Rena Okriyani       1817051029
 
 
 #include <iostream>
